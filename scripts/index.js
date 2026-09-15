bbfa3306f116a7b397ebdea4bd855b4dabd2f893
uay0n <hoshe1717@gmail.com>
2026-09-15 16:15:37 +0900

    베스트부분 시작

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,15 @@
 //hdex_index.js
+/* top 버튼 작동 */
 document.querySelector('.top_btn').addEventListener('click', () => {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
+});
+/* 베스트 상품 볼 때 누르기 */
+const rankBtn = document.querySelector('.rank_btn');
+const rankModal = document.querySelector('.rank_modal');
+
+rankBtn.addEventListener('click', () => {
+    rankModal.classList.toggle('active');
 });
